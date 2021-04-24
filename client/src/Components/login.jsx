63287d6b6c6d2de345b5ca34762bdcf73da91eb9
marhav <marhav95@gmail.com>
2021-04-24 14:05:21 +0200

    Adjusted the containers

--- client/src/Components/login.jsx
+++ client/src/Components/login.jsx
@@ -8,23 +8,21 @@ class Login extends Component {
             <React.Fragment>
                 <div className="loginContainer">
                     <div id="login_div" className="row">
-                        <div className="col-md-3">
-                        </div>
-                        <div className="col-md-6">
+                        <div className="col-md-12">
                             <div className="mainLogIn">
                                 <div className="form-signin">
                                     <div className="text-center mb-4">
                                         <h4>Sign in to store</h4>
                                     </div>
                                     <div className="form-label-group">
-                                        <input type="text" className="form-control m-2" placeholder="Username"
+                                        <input type="text" className="form-control mt-2" placeholder="Username"
                                                id="login_username"
                                                name="login_username" required="" autoFocus=""></input>
-                                        <input type="Password" className="form-control m-2" placeholder="Password"
+                                        <input type="Password" className="form-control mt-2" placeholder="Password"
                                                id="login_Password"
                                                name="login_Password" required="" autoFocus=""></input>
                                     </div>
-                                    <button className="btn btn-lg btn-primary btn-block m-2">Sign in
+                                    <button className="btn btn-lg btn-primary btn-block mt-2">Sign in
                                     </button>
                                 </div>
                             </div>
