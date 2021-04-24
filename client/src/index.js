b252d3918c5f35c59de0b4648e16f695ad0d2aba
MOHAMED HARATE <haratemo@gmail.com>
2021-04-24 12:17:10 +0200

    Slettet App.js

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min';
-import App from './App';
 import Home from "./Components/home";
 import reportWebVitals from './reportWebVitals';
 import $ from 'jquery';
