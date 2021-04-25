7c7a245763a8e4a4707b0ec071b8b7fa8cd2927f
marhav <marhav95@gmail.com>
2021-04-25 16:13:22 +0200

    Slightly changed the style of the login and register containers.
    Coded the logic for submitting and posting data from register.jsx.

--- client/src/Components/register.jsx
+++ client/src/Components/register.jsx
@@ -1,11 +1,53 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
+import axios from "axios";
 
 
-class Register extends Component{
+class Register extends Component {
     constructor(props) {
         super(props);
-        this.emailRef = React.createRef();
-        this.passwordRef = React.createRef();
+        this.state = {
+            firstname: '',
+            lastname: '',
+            email: '',
+            password: '',
+            zipCode: '',
+            streetName: '',
+            phone: '',
+        }
+
+        this.handleInputChange = this.handleInputChange.bind(this);
+    }
+
+    handleInputChange(event) {
+        const target = event.target;
+        const value = target.value;
+        const name = target.name;
+
+        this.setState({
+            [name]: value
+        })
+    }
+
+    async submitLoginDetails() {
+        alert("Submitted:\n" + this.state.firstname + "\n" + this.state.lastname + "\n" + this.state.email + "\n" +
+            this.state.password + "\n" + this.state.zipCode + "\n" + this.state.streetName + "\n" + this.state.phone)
+        try {
+            await axios({
+                method: 'post',
+                url: 'http://localhost:3001/signIn',
+                data: {
+                    firstname: this.state.firstname,
+                    lastname: this.state.lastname,
+                    email: this.state.email,
+                    password: this.state.password,
+                    zipCode: this.state.zipCode,
+                    streetName: this.state.streetName,
+                    phone: this.state.phone,
+                }
+            });
+        } catch (e){
+            console.error(e)
+        }
     }
 
     render() {
@@ -20,29 +62,29 @@ class Register extends Component{
                                         <h4>Register User</h4>
                                     </div>
                                     <div className="form-label-group">
-                                        <input type="text" className="form-control mt-2" placeholder="Firstname"
+                                        <input type="text" onChange={this.handleInputChange} className="form-control mt-2" placeholder="Firstname"
                                                id="register_firstname"
-                                               name="register_firstname" required="" autoFocus=""/>
-                                        <input type="text" className="form-control mt-2" placeholder="Lastname"
+                                               name="firstname" required="" autoFocus=""/>
+                                        <input type="text" onChange={this.handleInputChange} className="form-control mt-2" placeholder="Lastname"
                                                id="register_lastname"
-                                               name="register_lastname" required="" autoFocus=""/>
-                                        <input type="text" className="form-control mt-2" placeholder="Email"
+                                               name="lastname" required="" autoFocus=""/>
+                                        <input type="text" onChange={this.handleInputChange} className="form-control mt-2" placeholder="Email"
                                                id="register_email"
-                                               name="register_email" required="" autoFocus=""/>
-                                        <input type="Password" className="form-control mt-2" placeholder="Password"
+                                               name="email" required="" autoFocus=""/>
+                                        <input type="Password" onChange={this.handleInputChange} className="form-control mt-2" placeholder="Password"
                                                id="register_password"
-                                               name="register_password" required="" autoFocus=""/>
-                                        <input type="text" className="form-control mt-2" placeholder="Zip Code"
+                                               name="password" required="" autoFocus=""/>
+                                        <input type="text" onChange={this.handleInputChange} className="form-control mt-2" placeholder="Zip Code"
                                                id="register_zipCode"
-                                               name="register_zipCode" required="" autoFocus=""/>
-                                        <input type="text" className="form-control mt-2" placeholder="Street name"
+                                               name="zipCode" required="" autoFocus=""/>
+                                        <input type="text" onChange={this.handleInputChange} className="form-control mt-2" placeholder="Street name"
                                                id="register_streetName"
-                                               name="register_streetName" required="" autoFocus=""/>
-                                        <input type="text" className="form-control mt-2" placeholder="Phone"
+                                               name="streetName" required="" autoFocus=""/>
+                                        <input type="text" onChange={this.handleInputChange} className="form-control mt-2" placeholder="Phone"
                                                id="register_phone"
-                                               name="register_phone" required="" autoFocus=""/>
+                                               name="phone" required="" autoFocus=""/>
                                     </div>
-                                    <button className="btn btn-lg btn-primary btn-block mt-2">Register</button>
+                                    <button onClick={this.submitLoginDetails.bind(this)} className="btn btn-lg btn-primary btn-block mt-2">Register</button>
                                 </div>
                             </div>
                         </div>
@@ -52,4 +94,5 @@ class Register extends Component{
         );
     }
 }
+
 export default Register;
