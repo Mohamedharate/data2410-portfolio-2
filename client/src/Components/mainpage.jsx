015ec8d9e5caa6e2c82c5cc2f900399193535843
marhav <marhav95@gmail.com>
2021-04-25 15:22:18 +0200

    Endret elementer med class, til className. Er reservert i js og react må derfor bruke className isteden.
    Endret noen ellementer som ga feilmeldinger fordi de var lukket på feil måte.
    Det som skrives slik i js:
    <span class=""></span>
    Kan skrives slik i react:
    <span class="" />

--- client/src/Components/mainpage.jsx
+++ client/src/Components/mainpage.jsx
@@ -4,144 +4,144 @@ import React, { Component } from "react";
 class Mainpage extends Component{
     render() {
         return (
-          <div class="container">
+          <div className="container">
 
-            <div class="row">
+            <div className="row">
 
-              <div class="col-lg-3">
+              <div className="col-lg-3">
 
-                <h1 class="my-4">ShopMet</h1>
-                <div class="list-group">
-                  <a href="#" class="list-group-item">Category 1</a>
-                  <a href="#" class="list-group-item">Category 2</a>
-                  <a href="#" class="list-group-item">Category 3</a>
+                <h1 className="my-4">ShopMet</h1>
+                <div className="listclassName-group">
+                  <a href="#" className="list-group-item">Category 1</a>
+                  <a href="#" className="list-group-item">Category 2</a>
+                  <a href="#" className="list-group-item">Category 3</a>
                 </div>
 
               </div>
 
-              <div class="col-lg-9">
+              <div className="col-lg-9">
 
-                <div id="carouselExampleIndicators" class="carousel slide my-4" data-ride="carousel">
-                  <ol class="carousel-indicators">
-                    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-                    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-                    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                <div id="carouselExampleIndicators" className="carousel slide my-4" data-ride="carousel">
+                  <ol className="carousel-indicators">
+                    <li data-target="#carouselExampleIndicators" data-slide-to="0" className="active"/>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="1"/>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="2"/>
                   </ol>
-                  <div class="carousel-inner" role="listbox">
-                    <div class="carousel-item active">
-                      <img class="d-block img-fluid" src="http://placehold.it/900x350" alt="First slide"/>
+                  <div className="carousel-inner" role="listbox">
+                    <div className="carousel-item active">
+                      <img className="d-block img-fluid" src="http://placehold.it/900x350" alt="First slide"/>
                     </div>
-                    <div class="carousel-item">
-                      <img class="d-block img-fluid" src="http://placehold.it/900x350" alt="Second slide"/>
+                    <div className="carousel-item">
+                      <img className="d-block img-fluid" src="http://placehold.it/900x350" alt="Second slide"/>
                     </div>
-                    <div class="carousel-item">
-                      <img class="d-block img-fluid" src="http://placehold.it/900x350" alt="Third slide"/>
+                    <div className="carousel-item">
+                      <img className="d-block img-fluid" src="http://placehold.it/900x350" alt="Third slide"/>
                     </div>
                   </div>
-                  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span class="sr-only">Previous</span>
+                  <a className="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                    <span className="carousel-control-prev-icon" aria-hidden="true"/>
+                    <span className="sr-only">Previous</span>
                   </a>
-                  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span class="sr-only">Next</span>
+                  <a className="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                    <span className="carousel-control-next-icon" aria-hidden="true"/>
+                    <span className="sr-only">Next</span>
                   </a>
                 </div>
 
-                <div class="row">
+                <div className="row">
 
-                  <div class="col-lg-4 col-md-6 mb-4">
-                    <div class="card h-100">
-                      <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""/></a>
-                      <div class="card-body">
-                        <h4 class="card-title">
+                  <div className="col-lg-4 col-md-6 mb-4">
+                    <div className="card h-100">
+                      <a href="#"><img className="card-img-top" src="http://placehold.it/700x400" alt=""/></a>
+                      <div className="card-body">
+                        <h4 className="card-title">
                           <a href="#">Item One</a>
                         </h4>
                         <h5>$24.99</h5>
-                        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!</p>
+                        <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!</p>
                       </div>
-                      <div class="card-footer">
-                        <small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
+                      <div className="card-footer">
+                        <small className="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
                       </div>
                     </div>
                   </div>
 
-                  <div class="col-lg-4 col-md-6 mb-4">
-                    <div class="card h-100">
-                      <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""/></a>
-                      <div class="card-body">
-                        <h4 class="card-title">
+                  <div className="col-lg-4 col-md-6 mb-4">
+                    <div className="card h-100">
+                      <a href="#"><img className="card-img-top" src="http://placehold.it/700x400" alt=""/></a>
+                      <div className="card-body">
+                        <h4 className="card-title">
                           <a href="#">Item Two</a>
                         </h4>
                         <h5>$24.99</h5>
-                        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur! Lorem ipsum dolor sit amet.</p>
+                        <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur! Lorem ipsum dolor sit amet.</p>
                       </div>
-                      <div class="card-footer">
-                        <small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
+                      <div className="card-footer">
+                        <small className="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
                       </div>
                     </div>
                   </div>
 
-                  <div class="col-lg-4 col-md-6 mb-4">
-                    <div class="card h-100">
-                      <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""/></a>
-                      <div class="card-body">
-                        <h4 class="card-title">
+                  <div className="col-lg-4 col-md-6 mb-4">
+                    <div className="card h-100">
+                      <a href="#"><img className="card-img-top" src="http://placehold.it/700x400" alt=""/></a>
+                      <div className="card-body">
+                        <h4 className="card-title">
                           <a href="#">Item Three</a>
                         </h4>
                         <h5>$24.99</h5>
-                        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!</p>
+                        <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!</p>
                       </div>
-                      <div class="card-footer">
-                        <small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
+                      <div className="card-footer">
+                        <small className="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
                       </div>
                     </div>
                   </div>
 
-                  <div class="col-lg-4 col-md-6 mb-4">
-                    <div class="card h-100">
-                      <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""/></a>
-                      <div class="card-body">
-                        <h4 class="card-title">
+                  <div className="classNamecol-lg-4 col-md-6 mb-4">
+                    <div className="classNamecard h-100">
+                      <a href="#"><img className="classNamecard-img-top" src="http://placehold.it/700x400" alt=""/></a>
+                      <div className="classNamecard-body">
+                        <h4 className="classNamecard-title">
                           <a href="#">Item Four</a>
                         </h4>
                         <h5>$24.99</h5>
-                        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!</p>
+                        <p className="classNamecard-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!</p>
                       </div>
-                      <div class="card-footer">
-                        <small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
+                      <div className="classNamecard-footer">
+                        <small className="classNametext-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
                       </div>
                     </div>
                   </div>
 
-                  <div class="col-lg-4 col-md-6 mb-4">
-                    <div class="card h-100">
-                      <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""/></a>
-                      <div class="card-body">
-                        <h4 class="card-title">
+                  <div className="classNamecol-lg-4 col-md-6 mb-4">
+                    <div className="classNamecard h-100">
+                      <a href="#"><img className="classNamecard-img-top" src="http://placehold.it/700x400" alt=""/></a>
+                      <div className="classNamecard-body">
+                        <h4 className="classNamecard-title">
                           <a href="#">Item Five</a>
                         </h4>
                         <h5>$24.99</h5>
-                        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur! Lorem ipsum dolor sit amet.</p>
+                        <p className="classNamecard-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur! Lorem ipsum dolor sit amet.</p>
                       </div>
-                      <div class="card-footer">
-                        <small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
+                      <div className="classNamecard-footer">
+                        <small className="classNametext-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
                       </div>
                     </div>
                   </div>
 
-                  <div class="col-lg-4 col-md-6 mb-4">
-                    <div class="card h-100">
-                      <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""/></a>
-                      <div class="card-body">
-                        <h4 class="card-title">
+                  <div className="col-lg-4 col-md-6 mb-4">
+                    <div className="card h-100">
+                      <a href="#"><img className="card-img-top" src="http://placehold.it/700x400" alt=""/></a>
+                      <div className="card-body">
+                        <h4 className="card-title">
                           <a href="#">Item Six</a>
                         </h4>
                         <h5>$24.99</h5>
-                        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!</p>
+                        <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!</p>
                       </div>
-                      <div class="card-footer">
-                        <small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
+                      <div className="card-footer">
+                        <small className="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
                       </div>
                     </div>
                   </div>
