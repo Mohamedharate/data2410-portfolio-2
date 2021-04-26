c3facb65153ff73e1156e5d4e11fc52a76bcce6c
Tanja Aakerholt <tanja.aakerholt@hotmail.com>
2021-04-26 19:04:28 +0200

    cartActions

---
+++ client/src/actions/cartActions.js
@@ -0,0 +1,14 @@
+export const addToCart = (product) => {
+    return {
+
+    }
+}
+export const removeFromCart = (productId) => {
+
+}
+export const updateCartQuantity = (productId, quantity) => {
+
+}
+export const emptyCart = () => {
+
+}
