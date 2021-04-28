3054ba9665860f7bfde5119229791d26f92c5db2
MOHAMED HARATE <haratemo@gmail.com>
2021-04-28 20:19:05 +0200

    Har flyttet uploadmappen til serveren

--- server/routes/products.js
+++ server/routes/products.js
@@ -5,7 +5,7 @@ const multer = require('multer');
 
 const storage = multer.diskStorage({
     destination: function (req,file,cb){
-        cb(null,'./server/public/uploads');
+        cb(null,'./server/uploads');
     },
     filename: function (req,file,cb){
         cb(null,file.originalname)
