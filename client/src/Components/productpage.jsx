a55cddfb976fe6581d1d8661c6a31a7482890b42
Tanja Aakerholt <tanja.aakerholt@hotmail.com>
2021-05-02 12:41:05 +0200

    ryddet litt i koden

--- client/src/Components/productpage.jsx
+++ client/src/Components/productpage.jsx
@@ -1,12 +1,11 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 import axios from "axios";
 import {Link} from "react-router-dom";
-import Spinner from "react-bootstrap/Spinner";
 
-export default class Productpage extends Component{
+export default class Productpage extends Component {
     constructor(props) {
         super(props);
-        this.state = {products: [], name: "", price: 0, description: "", itemId: "", imageUrl: ""};
+        this.state = {name: "", price: 0, description: "", itemId: "", imageUrl: ""};
 
     }
 
@@ -14,13 +13,15 @@ export default class Productpage extends Component{
         const that = this;
         await axios({
             method: "get",
-            url: 'http://localhost:3001/api/products/get/'+this.props.match.params.itemId,
+            url: 'http://localhost:3001/api/products/get/' + this.props.match.params.itemId,
         }).then(function (response) {
             console.log("Data: ", response.data);
-            that.setState({products: response.data, name: response.data.name, price: response.data.price,
+            that.setState({
+                name: response.data.name, price: response.data.price,
                 description: response.data.description,
-            image: "data:image/png;base64,"+response.data.imageUrl[0].image,
-            itemId: response.data.itemId});
+                image: "data:image/png;base64," + response.data.imageUrl[0].image,
+                itemId: response.data.itemId
+            });
         }).catch(function (error) {
             if (!error.data) {
                 console.log(error.data)
@@ -31,15 +32,16 @@ export default class Productpage extends Component{
 
 
     render() {
-        const { price, image } = this.state;
-        return(
+        const {price} = this.state;
+        return (
             <div className="container">
-                    <div className="row">
-                        <div className="col-lg-3">
+                <div className="row">
+                    <div className="col-lg-3">
                         <h1 className="my-4">ShopMet</h1>
                         <div className="list-group">
                             <Link to="/category/coffee" className="list-group-item">Coffee</Link>
-                            <Link to="/category/machines" className="list-group-item">Coffee and espresso machines</Link>
+                            <Link to="/category/machines" className="list-group-item">Coffee and espresso
+                                machines</Link>
                             <Link to="/category/access" className="list-group-item">Accessories</Link>
                             <Link to="/" className="list-group-item">All</Link>
                         </div>
@@ -49,7 +51,7 @@ export default class Productpage extends Component{
                             <img className="card-img-top" src={this.state.image} alt="..."/>
                             <div className="card-body">
                                 <h3 className="card-title">{this.state.name}</h3>
-                                {Object.values(price).map((p) => (<h4 className="price" key="p">${p}</h4> ))}
+                                {Object.values(price).map((p) => (<h4 className="price" key="p">${p}</h4>))}
                                 <p className="card-text">{this.state.description}</p>
                                 <span className="text-warning">★ ★ ★ ★ ☆</span>
                                 4.0 stars
@@ -73,7 +75,8 @@ export default class Productpage extends Component{
                                     aperiam fugiat laborum. Sequi mollitia, necessitatibus quae sint natus.</p>
                                 <small className="text-muted">Posted by Anonymous on 3/1/21</small>
                                 <hr/>
-                                <Link to={'/addReview/'+this.state.itemId} className="btn btn-success" >Leave a Review</Link>
+                                <Link to={'/addReview/' + this.state.itemId} className="btn btn-success">Leave a
+                                    Review</Link>
                             </div>
                         </div>
                     </div>
