1b581d101887bab5d2e4c44d7a0d9a692ccccfcd
marhav <marhav95@gmail.com>
2021-05-03 18:32:39 +0200

    Made the design on the shopping cart.

--- client/src/Components/shoppingCart.jsx
+++ client/src/Components/shoppingCart.jsx
@@ -1,19 +1,43 @@
 import React, {Component} from "react";
+import ShoppingCartObject from "./shoppingCartObject";
+import axios from "axios";
 
 class ShoppingCart extends Component {
     state = {
-        cart_objects: []
+        cart_objects: [
+            {id: 1, name: 'Brød', quantity: 2,},
+            {id: 2, name: 'Bil', quantity: 2,},
+            {id: 3, name: 'Bord', quantity: 2,},
+            {id: 4, name: 'Blad', quantity: 2,},
+        ]
     }
 
     async getCartObjects() {
+        axios.get('')
+            .then(response => {
 
+            })
     }
 
     render() {
-
         return (
             <div className="shoppingCartContainer">
-
+                <div className="row">
+                    <div className="col-sm-5">
+                        <button className="btn btn-primary float-right mt-3">!Check out</button>
+                    </div>
+                    <div className="col-sm-7">
+                        <h4 className="text-center m-3">Shopping Cart</h4>
+                    </div>
+                </div>
+                <div className="shoppingCartObjectsContainer">
+                    {this.state.cart_objects.map(shoppingCartObject => (
+                        <ShoppingCartObject
+                            key={shoppingCartObject.id}
+                            shoppingCartObject={shoppingCartObject}
+                        />
+                    ))}
+                </div>
             </div>
         );
     }
