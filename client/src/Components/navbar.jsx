09c718d2e3b73ef5185c396407d8d843708eee62
marhav <marhav95@gmail.com>
2021-05-03 10:34:39 +0200

    Changed the path to the logo from relative to absolute.

--- client/src/Components/navbar.jsx
+++ client/src/Components/navbar.jsx
@@ -20,7 +20,7 @@ class Navbar extends Component{
                 <header>
                     <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
                         <div className="container-fluid">
-                            <img className="navbar-logo" src="./LogoMet.svg" alt=""/>
+                            <img className="navbar-logo" src="/LogoMet.svg" alt=""/>
                             <a className="navbar-brand"/>
                             <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
                                     data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false"
