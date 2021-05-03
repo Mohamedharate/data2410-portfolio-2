b21b8441ebab56ce8d555e71e79818c5ed8d1edc
Tanja Aakerholt <tanja.aakerholt@hotmail.com>
2021-05-03 14:53:45 +0200

    begynte på siden for tidlgere orders, må definere route i backend først

--- client/src/Components/orders.jsx
+++ client/src/Components/orders.jsx
@@ -1,9 +1,56 @@
 import React, {Component} from "react";
+import axios from "axios";
 
 class Orders extends Component{
-    render() {
-        return
+
+    constructor(props) {
+        super(props);
+        this.state = {user: "", orders: []};
     }
 
+    async getOrder(){
+        await axios({
+            method: "get",
+            url: "http://localhost:3001/api/orders/get/"+this.props.match.email,
+        }).then(function (response){
+        this.setState({email: response.data.email, orderNumber: response.data.orderNumber,
+        products: response.data.products, total: response.data.total})
+        }).catch(function (error){
+            console.log(error.data)
+        });
+    }
+
+
+
+    render() {
+        const { orderNumber } = this.state;
+        return(
+
+            <div className="previous-orders">
+                <table className="table">
+
+                    <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Total</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {orderNumber.map((order)=>(
+                        <tr key={order}>
+                            <td>{order}</td>
+                            <td>{order.total}</td>
+                        </tr>
+                    ))}
+                    </tbody>
+                </table>
+            </div>
+        )
+
+
+    }
+
+
 }
+
 export default Orders
