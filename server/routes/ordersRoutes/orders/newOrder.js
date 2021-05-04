a4c05729f945c1a3c637add8438fd56e9df39dba
marhav <marhav95@gmail.com>
2021-05-04 16:06:17 +0200

    The check out button is now working. Got an unexpected error from server when i tried to access new order. However the order was placed.
    TODO:
    Add some feedbacks to the shoppingCart, so the user get necessary feedback when trying to place an order.

--- server/routes/ordersRoutes/orders/newOrder.js
+++ server/routes/ordersRoutes/orders/newOrder.js
@@ -8,6 +8,51 @@ const send = require('../../../sendMail/sendMailReceipt');
 const formatReceipt = require('../../../sendMail/formatReceipt');
 
 
+router.post('/', async (req, res) => {
+
+    const userId = req.session.userId;
+
+    const user = await User.findOne({_id: userId});
+
+    if (user) {
+        if (user.cart.length < 1) return res.status(404).json({Error: "No cart"})
+
+        else {
+            let tot = 0;
+            user.cart.forEach(item => {
+                tot = parseFloat(tot) + parseFloat(item.total);
+            })
+
+            const order = new Order({
+                user: user.email,
+                products: user.cart,
+                firstName: req.body.firstName,
+                lastName: req.body.lastName,
+                email: req.body.email,
+                country: req.body.country,
+                zipCode: req.body.zipCode,
+                city: req.body.city,
+                street: req.body.street,
+                phoneNumber: req.body.phoneNumber,
+                total: parseFloat(tot)
+            })
+            await User.updateOne({email: user.email}, {$push: {orders: order}});
+            await User.updateOne(
+                {email: user.email},
+                {$set: {[`cart`]: []}});
+            await order.save()
+                .then(data => {
+                    send(user, formatReceipt(user,order,tot))
+                    res.status(200).json({Message: "The order has been registered and a receipt is sent to the user"})
+                })
+                .catch(error => {
+                    res.status(400).json({Error:error.toString()});
+                })
+        }
+    }
+    else return res.status(404).json({Error: "User not found."})
+});
+
 router.post('/:email', async (req, res) => {
     const email = req.params.email;
 
