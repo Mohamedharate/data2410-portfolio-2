0f280182589edd6d403478b2e1d6a037d4125f14
marhav <marhav95@gmail.com>
2021-05-11 10:38:21 +0200

    Optimized register.jsx. Added google button for signing up with google. Added success alert for serverfeedback after registerging user. This has not been tested. Added loading spinner to the sign up button.

--- client/src/Components/register.jsx
+++ client/src/Components/register.jsx
@@ -1,6 +1,8 @@
 import React, {Component} from "react";
 import axios from "axios";
 import DangerFeedback from "./dangerFeedback";
+import SuccessFeedback from "./successFeedback";
+import LoadingSpinnerBtn from "./LoadingSpinnerBtn";
 
 
 class Register extends Component {
@@ -20,7 +22,9 @@ class Register extends Component {
         zipCode: '',
         street: '',
         phoneNumber: '',
+        toggle_loading: false,
         toggle_error_feedback: false,
+        toggle_success_feedback: false,
         feedback_text: '',
     }
 
@@ -35,17 +39,9 @@ class Register extends Component {
         })
     }
 
-    handleSubmit = (event) => {
+    handleSubmit = async event => {
         event.preventDefault();
-        this.submitUserDetails()
-            .then(response => {
-            }).catch(error => {
-        })
-    }
-
-    async submitUserDetails() {
-        const that = this;
-
+        this.setState({toggle_loading: true})
         await axios({
             method: 'post',
             url: 'http://localhost:3001/api/register',
@@ -60,36 +56,53 @@ class Register extends Component {
                 street: this.state.street,
                 phoneNumber: this.state.phoneNumber,
             }
-        }).then(function (response) {
-            console.log("Data:", response.data.Message);
-            console.log("Status:", response.status);
-            that.setState({toggle_error_feedback: false})
-            that.setState({feedback_text: response.data.Message})
-            that.props.registerCallback()
-        }).catch(function (error) {
-            if (error.response) {
-                console.log(error.response.data.Error);
-                console.log(error.response.status);
-                that.setState({toggle_error_feedback: true})
-                that.setState({feedback_text: error.response.data.Error})
-            } else if (error.request) {
-                console.log(error.request);
-            } else {
-                console.log('Error', error.message);
-            }
-        });
+        }).then(response => {
+            this.setState({
+                toggle_loading: false,
+                toggle_success_feedback: true,
+                toggle_error_feedback: false,
+                feedback_text: response.data.Message
+            })
+        }).catch(error => {
+            this.setState({
+                toggle_loading: false,
+                toggle_success_feedback: false,
+                toggle_error_feedback: true,
+                feedback_text: error.response.data.Error
+            })
 
+        });
     }
 
     render() {
         return (
             <div className="registerContainer">
                 <div id="login_div" className="row">
-                    <div className="col-md-12">
-                        <form onSubmit={this.handleSubmit} className="form-signin">
-                            <div className="text-center mb-4">
-                                <h4>Register User</h4>
+                    <div className="col-md-12 text-center">
+                        <div className="mb-4">
+                            <h4>Register User</h4>
+                        </div>
+                    </div>
+                </div>
+                <div className="row text-center">
+                    <div className="col-12">
+                        <a href="http://localhost:3001/auth/google" className="google-btn">
+                            <div className="google-icon-wrapper">
+                                <img className="google-icon"
+                                     src="/btn_google_light_normal_ios.svg"/>
                             </div>
+                            <p className="btn-text"><b>Sign up with google</b></p>
+                        </a>
+                        <div className="separator">
+                            <div className="line"/>
+                            <h5>or</h5>
+                            <div className="line"/>
+                        </div>
+                    </div>
+                </div>
+                <div className="row mt-2">
+                    <div className="col-12">
+                        <form onSubmit={this.handleSubmit} className="form-signin">
                             <div className="form-label-group">
                                 <input type="text" onChange={this.handleInputChange}
                                        className="form-control mt-2" placeholder="Firstname"
@@ -109,7 +122,8 @@ class Register extends Component {
                                        pattern="^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}"
                                        title="Password must contain at least 1 upper and lower case letter, 1 number and be at least 8 characters long."
                                        name="password" required autoFocus=""/>
-                                <select name="country" onChange={this.handleInputChange} className="custom-select my-1 mr-sm-2 mt-2" required>
+                                <select name="country" onChange={this.handleInputChange}
+                                        className="custom-select my-1 mr-sm-2 mt-2" required>
                                     <option value="" defaultValue="">Choose...</option>
                                     <option value="Norway">Norway</option>
                                     <option value="Sweden">Sweden</option>
@@ -137,10 +151,13 @@ class Register extends Component {
                                        <Country code(optional)> <Number(At least 7 digits>."
                                        name="phoneNumber" required autoFocus=""/>
                             </div>
-                            <button
-                                type="submit"
-                                className="btn btn-lg btn-primary btn-block mt-2">Sign Up!
-                            </button>
+                            {this.state.toggle_loading ? <LoadingSpinnerBtn/> :
+                                <button
+                                    type="submit"
+                                    className="btn btn-lg btn-primary btn-block mt-2">Sign Up!
+                                </button>}
+                            {this.state.toggle_success_feedback &&
+                            <SuccessFeedback feedback_success_text={this.state.feedback_text}/>}
                             {this.state.toggle_error_feedback &&
                             <DangerFeedback feedback_error_text={this.state.feedback_text}/>}
                         </form>
