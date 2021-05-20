eef0060b98c036f4fc689d869ffce26353ad5140
Tanja Aakerholt <tanja.aakerholt@hotmail.com>
2021-05-20 22:02:47 +0200

    Grønn spinner

---
+++ client/src/Components/Spinners/LoadingSpinnerLargeGeneral.jsx
@@ -0,0 +1,10 @@
+import React, {Component} from "react";
+
+class LoadingSpinnerLargeGeneral extends Component{
+    render() {
+        return(
+            <div className="spinner-border text-success m-5" role="status" />
+        )
+    }
+}
+export default LoadingSpinnerLargeGeneral;
